8dfe3f5ad5f67b69079e043df09a54cdbfc7c4c5
Shirley-April <shirleyapril23@gmail.com>
2023-12-31 13:11:48 +0300

    display all products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { products } from '../products';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
