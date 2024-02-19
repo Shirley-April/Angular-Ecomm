1c70d3efd99caaa6c9c10c919f0de8109a10321f
Shirley-April <shirleyapril23@gmail.com>
2024-02-19 21:59:48 +0300

    update cart component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { Product } from '../products';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf, RouterLink],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
@@ -13,17 +15,17 @@ export class CartComponent {
   constructor(private cartService: CartService) {}
 
   cartItems = this.cartService.getCartItems();
+  cartTotal = this.cartService.calculateTotalPrice();
 
   removeItem(id: number) {
     this.cartService.removeItem(id);
   }
 
-  addQuantity(id: number) {
-    this.cartService.addQuantity(id)
+  addQuantity(item: Product) {
+    this.cartService.addToCart(item);
   }
 
   decreaseQuantity(id: number) {
-    this.cartService.decreaseQuantity(id)
+    this.cartService.decreaseQuantity(id);
   }
 }
-
