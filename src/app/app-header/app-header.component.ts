b13ec8bed176068fd8aadb51ec5aeb1aa66e3ee9
Shirley-April <shirleyapril23@gmail.com>
2024-02-19 21:58:33 +0300

    header styling

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-app-header',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './app-header.component.html',
-  styleUrl: './app-header.component.css'
+  styleUrl: './app-header.component.css',
 })
 export class AppHeaderComponent {
+  constructor(private cartService: CartService) {}
 
+  cartItems = this.cartService.getCartItems();
 }
